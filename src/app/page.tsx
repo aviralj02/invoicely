b2bd48930f46fb1cd6a7d7c212ef4e64ae849657
Aviral Jain <aviraljain1311@gmail.com>
2024-10-21 02:30:41 +0530

    refactor: :coffin: remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import PageWrapper from "@/components/PageWrapper";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {
   OrganizationSwitcher,
-  SignInButton,
   SignedIn,
   SignedOut,
   UserButton,
